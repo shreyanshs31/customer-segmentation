7bd05975819d2af8dd49e5005c13fc93c32abfb5
shreyanshs31 <shreyansh.shukla.3101@gmail.com>
2025-12-10 15:47:32 +0530

    comments added

--- customerSegmentation.py
+++ customerSegmentation.py
@@ -3,47 +3,49 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from mpl_toolkits.mplot3d import Axes3D
 
 df = pd.read_csv('Mall_Customers.csv')
 # print(df.describe())
 # print(df.info())
 
+#*****Univariate Analysis*********
 # plt.figure(figsize=(10,6))
 # plt.hist(x=df['Age'], bins=20, color='skyblue', edgecolor='black')
-
 # plt.title('Distribution of Age')
 # plt.xlabel('age')
 # plt.ylabel('Frequency')
+# plt.savefig('ageHistogram.png')
 
-# plt.savefig('ageHistogram20bins.png')
-
-
+#*****Univariate Analysis*********
 # plt.hist(x=df['Annual Income (k$)'], bins=20, color='skyblue', edgecolor='black')
 # plt.title('Distribution of Annual Income')
 # plt.xlabel('Income')
 # plt.ylabel('Frequency')
 # plt.savefig('annualIncomeHistogram.png')
 
-
+#*****Univariate Analysis*********
 # plt.hist(x=df['Spending Score (1-100)'], bins=20, color='skyblue', edgecolor='black')
 # plt.title('Distribution of Spending Score')
 # plt.xlabel('Spending Score')
 # plt.ylabel('Frequency')
 # plt.savefig('spendingScoreHistogram.png')
 
+#*****Univariate Analysis*********
 # plt.hist(x=df['Gender'], bins=2, color='skyblue', edgecolor='black')
 # plt.title('Distribution of Gender')
 # plt.xlabel('Gender')
 # plt.ylabel('Frequency')
 # plt.savefig('genderHistogram.png')
 
-
+#*****Bivariate Analysis*********
 # plt.scatter(x=df['Annual Income (k$)'], y=df['Spending Score (1-100)'], color='skyblue', edgecolors='black')
 # plt.title('Scatter plot')
 # plt.xlabel('Annual Income')
 # plt.ylabel('Spending Score')
 # plt.savefig('scatterAnnualIncomevsSpendingScore')
 
+#*******Heatmap********
 # numeric_df= df.select_dtypes(include=['number'])
 # corr_matrix = numeric_df.corr()
 # plt.figure(figsize=(10,8))
@@ -52,7 +54,7 @@ df = pd.read_csv('Mall_Customers.csv')
 # plt.savefig('correlation_heatmap.png')
 
 
-#************Feature Selection*************
+#************Feature Selection two variables*************
 # with Annual Income and Spending Score
 
 # X=df[['Annual Income (k$)', 'Spending Score (1-100)']]
@@ -60,7 +62,7 @@ df = pd.read_csv('Mall_Customers.csv')
 # X_scaled=scaler.fit_transform(X)
 # print(X_scaled[:5])
 
-#***********Feature Selection*************
+#***********Feature Selection three variables*************
 # with Annual Income, Spending Score and Age
 
 X=df[['Annual Income (k$)', 'Spending Score (1-100)', 'Age']]
@@ -68,7 +70,7 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 # print(X_scaled[:5])
 
-
+#******* Elbo Method**********
 # wcss = []
 # for i in range(1, 11):
 #     kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
@@ -90,12 +92,27 @@ cluster_labels = kmeans.fit_predict(X_scaled)
 df['Cluster'] = cluster_labels
 # print(df.head())
 
-plt.figure(figsize=(10, 6))
-sns.scatterplot(data=df, x='Annual Income (k$)', y='Spending Score (1-100)', hue='Cluster', palette='viridis', s=100)
-
+#*******2D figure plot*******
+# plt.figure(figsize=(10, 6))
+# sns.scatterplot(data=df, x='Annual Income (k$)', y='Spending Score (1-100)', hue='Cluster', palette='viridis', s=100)
 # plt.title('Customer Segments')
 # plt.xlabel('Annual Income')
 # plt.ylabel('Spending Score')
 # plt.legend(title='Cluster')
-
 # plt.savefig('cluster_map.png')
+
+#*******3D figure plot*******
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+xs = df['Age']
+ys = df['Annual Income (k$)']
+zs = df['Spending Score (1-100)']
+scatter = ax.scatter(xs, ys, zs, c=df['Cluster'], cmap='viridis', s=50) # type: ignore
+ax.set_xlabel('Age')
+ax.set_ylabel('Annual Income')
+ax.set_zlabel('Spending Score')
+legend1 = ax.legend(*scatter.legend_elements(), title="Cluster")
+ax.add_artist(legend1)
+plt.title('3D Customer Segmentation')
+plt.savefig('3d_cluster_map.png')
+
